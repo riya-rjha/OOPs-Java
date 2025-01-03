b7aa5e117577596f3412c63c551e65677d5e88d2
riya-rjha <riyaranjanjha2006@gmail.com>
2025-01-03 15:09:25 +0530

    Add implementation of Car interface and its subclasses with detailed methods

--- Interfaces/Interface.java
+++ Interfaces/Interface.java
@@ -1,7 +1,110 @@
 package Interfaces;
 
 public class Interface {
+
+    public interface Car {
+        int maxSpeed = 0;
+
+        void engineStart();
+
+        void musicPlayerStart();
+
+        void engineStop(int maxSpeed);
+    }
+
+    public interface InnerCar extends Car {
+        void noOfSeats(int seats);
+    }
+
+    public class ElectricalEngine implements InnerCar {
+        @Override
+        public void engineStart() { // added public as in interface all methods are public by default
+            System.out.println("Electrical engine car started");
+        }
+
+        @Override
+        public void musicPlayerStart() {
+            System.out.println("Electrical engine car music player stopped");
+        }
+
+        @Override
+        public void engineStop(int maxSpeed) {
+            System.out.println("Electrical engine car stopped at " + maxSpeed + " km/hr");
+        }
+
+        @Override
+        public void noOfSeats(int seats) {
+            System.out.println("This electrical engine car is a " + seats + " seater");
+        }
+
+        public MechanicalEngine changeCarType(ElectricalEngine electricalEngine) {
+            System.out.println("Electrical engine car changed to Mechanical engine");
+            return new MechanicalEngine();
+        }
+    }
+
+    public class MechanicalEngine implements InnerCar {
+        @Override
+        public void engineStart() { // added public as in interface all methods are public by default
+            System.out.println("Mechanical engine car started");
+        }
+
+        @Override
+        public void musicPlayerStart() {
+            System.out.println("Mechanical engine car music player stopped");
+        }
+
+        @Override
+        public void engineStop(int maxSpeed) {
+            System.out.println("Mechanical engine car stopped at " + maxSpeed + " km/hr");
+        }
+
+        @Override
+        public void noOfSeats(int seats) {
+            System.out.println("This mechanical engine car is a " + seats + " seater");
+        }
+    }
+
+    public interface A {
+        int num = 32;
+    }
+
+    public interface B {
+        int num = 24;
+    }
+
+    // Multiple Inheritance
+
+    public class C implements A, B {
+        C() {
+            super();
+        }
+
+        public int valueOfNumInA() {
+            return A.num;
+        }
+        public int valueOfNumInB() {
+            return B.num;
+        }
+    }
+
     public static void main(String[] args) {
-        
+        Interface interface1 = new Interface();
+        // InnerCar innerCar = interface1.new InnerCar(); // instantiation of interfaces
+        // does not take place
+
+        ElectricalEngine electricalEngine = interface1.new ElectricalEngine();
+        electricalEngine.engineStart();
+        electricalEngine.engineStop(319);
+        electricalEngine.noOfSeats(4);
+
+        MechanicalEngine mechanicalEngine = electricalEngine.changeCarType(electricalEngine);
+        mechanicalEngine.engineStart();
+        mechanicalEngine.engineStop(2);
+
+        C c = interface1.new C();
+        System.out.println(c.valueOfNumInA());
+        System.out.println(c.valueOfNumInB());
+
     }
 }
--- Interfaces/Interface.md
+++ Interfaces/Interface.md
@@ -4,8 +4,14 @@
 
 - Instances in an interface are implicitly declared as final, static and public. Classes are declared implcitly as public and abstract. (Java 8)
 
-- All methods in Interfaces are public by default and unlike Abstraction, here keyword implements is used instead of extends. Apart from this, Abstraction does not allow more than one child class of a parent class whereas using Interfaces, we can define more than one child class for a single parent. 
+- All methods in Interfaces are public by default and unlike Abstraction, here keyword **implements** is used instead of extends. Apart from this, Abstraction does not allow more than one child class of a parent class whereas using Interfaces, we can define more than one child class for a single parent. 
 
 - Two classes that are unrelated to each other and can also implement the same parent using interfaces, unlike abstract classes, where a hierarchical structure is followed due to this: Parent -> Class -> Subclass and etc.
 
 - Interfaces can sometimes cause performance critical codes, such as where due to the reason inheritances objects cannot be instantiated, so it needs to be called through method and this increases overhead.
+
+- If A is an interface and B implements A, and C implements B, then C has to override both methods of A as well as B given the fact that the methods have only been stated in B and not been overridden by it.
+
+- In order to inherit one interface to another, we use **extends** keyword, and when we inherit a class from another interface, we use **implements**.
+
+- Like Abstraction, where you cannot instantiate objects of an abstract class, similarly, interfaces cannot instantiate their objects. However, the interfaces can be used as referential classes and this allows them to access only their own methods. 
